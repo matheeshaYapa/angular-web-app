56a067f1de75d161824952ea8524cca926bcd7c8
matheeshaYapa <ylmatheesha@gmail.com>
2019-02-02 19:00:02 +0530

    ngFor, ngIf, ngStyle and ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    ParagraphComponent
   ],
   imports: [
     BrowserModule,
